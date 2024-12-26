05c1a1c6f923ec331580c9dd9ef60fd42d64390c
Hasitha Saubhagya Dassanayaka <79803555+HasithaSawbhagy@users.noreply.github.com>
2024-12-27 00:24:54 +0530

    POM design pattern added & Testng added
    Testng added, POM design pattern added, Login feature tested

--- UITesting/src/test/java/org/example/stepdefs/LoginPageStepDef.java
+++ UITesting/src/test/java/org/example/stepdefs/LoginPageStepDef.java
@@ -1,53 +1,63 @@
-package com.example.UITesting;
+// Login is done by 204031F Dassanayaka H.D.H.S.
+package org.example.stepdefs;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
+import org.example.pages.HomePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.junit.jupiter.api.Assertions;
+import org.testng.Assert;
+import org.example.pages.LoginPage;
+
 import java.time.Duration;
 
-
-public class LoginSteps {
-
-    @Autowired
+public class LoginPageStepDef {
     private WebDriver driver;
+    private LoginPage loginPage;
+    private HomePage homePage;
+
+    @Before
+    public void setup(){
+        // ****Set the path to your chromedriver executable here****
+        System.setProperty("webdriver.chrome.driver", "C:/Users/hasit/Downloads/chromedriver-win64/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown(){
+        if(driver!=null){
+            driver.quit();
+        }
+    }
 
 
     @Given("I am in the login page of the OrangeHRM Application")
     public void iAmInTheLoginPageOfTheOrangeHRMApplication() {
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        loginPage = new LoginPage(driver);
     }
-
 
     @When("I enter valid credentials")
     public void iEnterValidCredentials() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement usernameField = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("username")));
         WebElement passwordField = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("password")));
-        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']")));
-
-        usernameField.sendKeys("Admin");
-        passwordField.sendKeys("admin123");
-        loginButton.click();
+        WebElement loginButton =  wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']")));
+        homePage=  loginPage.login("Admin", "admin123");
     }
-
 
     @Then("I should be taken to the Overview page")
     public void iShouldBeTakenToTheOverviewPage() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.titleIs("OrangeHRM"));
-        String expectedTitle = "OrangeHRM";
-        String actualTitle = driver.getTitle();
-        if (!actualTitle.equals(expectedTitle)) {
-            Assertions.fail("Login failed. Expected title is " + expectedTitle + ", but the actual title was " + actualTitle);
-        }
-        Assertions.assertEquals(expectedTitle,actualTitle);
+        Assert.assertEquals(driver.getTitle(), "OrangeHRM");
         driver.quit();
     }
 }
--- UITesting/src/test/resources/features/Login.feature
+++
@@ -1,9 +0,0 @@
-Feature: Login Functionality
-  In order to do internet banking
-  As a valid Para Bank customer
-
-Scenario: Login Successful
-
-Given I am in the login page of the OrangeHRM Application
-When I enter valid credentials
-Then I should be taken to the Overview page
