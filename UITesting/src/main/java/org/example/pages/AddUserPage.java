4c47fcb3323d64a5ddd767c1f90de2abb0359d31
VidathAmarasekara <vidathamarasekara99@gmail.com>
2024-12-28 14:55:36 +0530

    set the waiting

--- UITesting/src/main/java/org/example/pages/AddUserPage.java
+++ UITesting/src/main/java/org/example/pages/AddUserPage.java
@@ -18,23 +18,17 @@ public class AddUserPage {
     // Locators for the "Add User" page
     @FindBy(xpath = "//div[@class='oxd-select-wrapper']//div[contains(@class, 'oxd-select-text-input') and text()='-- Select --']")
     private WebElement userRoleDropdown;
-
     @FindBy(xpath = "//label[text()='Status']/following::div[contains(@class,'oxd-select-text')][1]")
     private WebElement statusDropdown;
-
     // Locators for the "Add User" page
     @FindBy(xpath = "//input[@placeholder='Type for hints...']")
     private WebElement employeeNameField;
-
     @FindBy(xpath = "//input[@class='oxd-input oxd-input--active' and @autocomplete='off']")
     private WebElement usernameField;
-
     @FindBy(xpath = "//label[text()='Password']/following::input[@type='password' and @autocomplete='off'][1]")
     private WebElement passwordField;
-
     @FindBy(xpath = "//label[text()='Confirm Password']/following::input[@type='password' and @autocomplete='off'][1]")
     private WebElement confirmPasswordField;
-
     @FindBy(xpath = "//button[@type='submit']")
     private WebElement saveButton;
 
@@ -45,7 +39,6 @@ public class AddUserPage {
     }
 
     // Method to select a user role
-    // Method to select a user role
     public void selectUserRole(String role) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
@@ -53,26 +46,21 @@ public class AddUserPage {
         wait.until(ExpectedConditions.elementToBeClickable(userRoleDropdown)).click();
 
         // Wait until the dropdown options container is visible
-        WebElement dropdownContainer = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.xpath("//div[contains(@class, 'oxd-select-dropdown') and contains(@class, '--positon-bottom')]")
+        WebElement roleOption = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath(".//div[@role='option' and contains(.,'" + role + "')]")
         ));
 
-        // Locate the specific role option within the dropdown
-        WebElement roleOption = dropdownContainer.findElement(
-                By.xpath(".//div[@role='option' and contains(.,'" + role + "')]")
-        );
-
-        // Ensure the option is visible and click it
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", roleOption);
-        wait.until(ExpectedConditions.elementToBeClickable(roleOption)).click();
+        // Click the desired option
+        roleOption.click();
     }
 
     public void selectStatus(String status) {
-        // Click the Status dropdown to open it
-        statusDropdown.click();
-
-        // Wait for the dropdown options to be visible
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Wait until the dropdown is clickable and then click it to open
+        wait.until(ExpectedConditions.elementToBeClickable(statusDropdown)).click();
+
+        // Wait until the dropdown options container is visible
         WebElement optionToSelect = wait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("//div[@role='option' and span[text()='" + status + "']]")
         ));
@@ -81,8 +69,6 @@ public class AddUserPage {
         optionToSelect.click();
     }
 
-
-    // Method to enter employee name
     // Method to enter employee name
     public void enterEmployeeName(String employeeName) {
         // Input the employee name into the text box
@@ -100,21 +86,42 @@ public class AddUserPage {
 
     // Method to enter username
     public void enterUsername(String username) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Wait for the username field to be visible
+        wait.until(ExpectedConditions.visibilityOf(usernameField));
+
+        // Input the username
         usernameField.sendKeys(username);
     }
 
-    // Method to enter password
     public void enterPassword(String password) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Wait for the password field to be visible
+        wait.until(ExpectedConditions.visibilityOf(passwordField));
+
+        // Input the password
         passwordField.sendKeys(password);
     }
 
-    // Method to enter confirm password
     public void enterConfirmPassword(String confirmPassword) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Wait for the password field to be visible
+        wait.until(ExpectedConditions.visibilityOf(confirmPasswordField));
+
+        // Input the password
         confirmPasswordField.sendKeys(confirmPassword);
     }
 
-    // Method to click the save button
     public void clickSave() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Wait for the save button to be clickable
+        wait.until(ExpectedConditions.elementToBeClickable(saveButton));
+
+        // Click the save button
         saveButton.click();
     }
 }
--- UITesting/src/test/resources/features/adduser.feature
+++ UITesting/src/test/resources/features/adduser.feature
@@ -2,5 +2,5 @@ Feature: Create User in HRM System
 
   Scenario: Create a new user with role Admin
     Given I am logged into the HRM system
-    When I create a new user with the role "Admin", status "Enabled", employee name "Joseph  Evans", username "john_evans", password "Johnevans123#", and confirm password "Johnevans123#"
+    When I create a new user with the role "Admin", status "Enabled", employee name "Luther Robin Bechtelar", username "john_robin", password "Johnevans123#", and confirm password "Johnevans123#"
     Then The user should be created successfully
