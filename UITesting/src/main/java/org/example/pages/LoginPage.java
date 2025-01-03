6537b2e73fae1e7d134b87673fef6b41a8089091
VidathAmarasekara <vidathamarasekara99@gmail.com>
2025-01-03 16:37:38 +0530

    UI Check
    Co-Authored-By: Ahinsa Kavindi <105373867+AhinsaKavindi22@users.noreply.github.com>
    Co-Authored-By: ChamudiChanuki <97381741+ChamudiChanuki@users.noreply.github.com>
    Co-Authored-By: Hasitha Saubhagya Dassanayaka <79803555+HasithaSawbhagy@users.noreply.github.com>
    Co-Authored-By: NithyaAdhi <113980392+NithyaAdhi@users.noreply.github.com>
    Co-Authored-By: bmkl23 <106014048+bmkl23@users.noreply.github.com>

--- UITesting/src/main/java/org/example/pages/LoginPage.java
+++ UITesting/src/main/java/org/example/pages/LoginPage.java
@@ -1,4 +1,3 @@
-
 // Login is done by 204031F Dassanayaka H.D.H.S.
 package org.example.pages;
 
@@ -6,12 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class LoginPage extends BasePage {
+    //removed the driver field as it is now in BasePage
 
     @FindBy(name = "username")
     private WebElement usernameField;
@@ -28,20 +24,14 @@ public class LoginPage extends BasePage {
     }
 
     public void enterUsername(String username) {
-        new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.visibilityOf(usernameField));
         usernameField.sendKeys(username);
     }
 
     public void enterPassword(String password) {
-        new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.visibilityOf(passwordField));
         passwordField.sendKeys(password);
     }
 
     public HomePage clickLoginButton() {
-        new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.elementToBeClickable(loginButton));
         loginButton.click();
         return new HomePage(driver);
     }
